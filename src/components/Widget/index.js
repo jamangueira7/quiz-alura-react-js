e1545bf192f6a6c7db6ba7122b91bd432ce66442
João Mangueira <jamjs7@gmail.com>
2021-01-27 15:07:07 -0300

    Ajustando component Widget

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 const Widget = styled.div`
     margin-top: 24px;
     margin-bottom: 24px;
-    border: 1px solid #4CAF50;
-    background-color: #1C1814;
+    border: 1px solid ${({ theme }) => theme.colors.primary};
+    background-color: ${({ theme }) => theme.colors.mainBg};
     border-radius: 4px;
     overflow: hidden;
     
