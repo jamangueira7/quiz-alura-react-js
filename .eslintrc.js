20d57bf51eafd23a40c17c2364e1818d4af074f3
João Mangueira <jamjs7@gmail.com>
2021-01-27 20:45:38 -0300

    Ajustano regra do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,11 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-props-no-spreading': ['error', {
+      html: 'ignore',
+      custom: 'ignore',
+      explicitSpread: 'ignore',
+      exceptions: [''],
+    }],
   },
 };
