4717b2ee101b68a7cea3130ad04eff1f7511b086
João Mangueira <jamjs7@gmail.com>
2021-01-27 14:15:39 -0300

    usando DB json e mudando o thema

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import db from '../db.json';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const GlobalStyle = createGlobalStyle`
+ 
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const { theme } = db;
+
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
-
-export default MyApp
