6e5718c90305e1781e7969b783158da75d93fd86
João Mangueira <jamjs7@gmail.com>
2021-01-29 12:08:23 -0300

    codigo para pegar json de outros projetos

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -9,7 +9,29 @@ export default function QuizDaGaleraPage() {
 }
 
 export async function getServerSideProps(context) {
-    return {
-        props: {},
-    };
+    const [projectName, githubUser] = context.query.id.split('___');
+
+    try {
+        const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app/api/db`)
+            .then((respostaDoServer) => {
+                if (respostaDoServer.ok) {
+                    return respostaDoServer.json();
+                }
+                throw new Error('Falha em pegar os dados');
+            })
+            .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto)
+        // .catch((err) => {
+        //   // console.error(err);
+        // });
+
+        // console.log('dbExterno', dbExterno);
+        // console.log('Infos que o Next da para nós', context.query.id);
+        return {
+            props: {
+                dbExterno,
+            },
+        };
+    } catch(err) {
+        throw new Error(err);
+    }
 }
